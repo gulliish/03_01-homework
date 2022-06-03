fba089cd45a0f3beec6a597e694c012b13ca7ff6
gulliish <gulmiraabdujalilova@gmail.com>
2022-06-03 21:19:29 +0600

    final commit

---
+++ script.js
@@ -0,0 +1,27 @@
+//1
+const a = 10; 
+const b = 3;
+const module = a % b;
+console.log(module);
+
+//2
+const st = 2 ** 10;
+console.log(st);
+
+//3
+let c = "1";
+let d = "2";
+const e = " белых медведей";
+console.log(c+d+e);
+
+//4 
+let f = 5; 
+let j = 10;
+let h ; 
+h = f + j--; 
+console.log(h);
+
+//5
+//-390. const x = 15 * 4 + 25 - 55;
+const x = 15 * (4 + 25 - 55);
+console.log(x);
